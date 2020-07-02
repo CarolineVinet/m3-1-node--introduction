98ed2437b5c64d445f3bcdbc771ffd3c33b4c7aa
Caroline Vinet <carovinet@gmail.com>
2020-07-02 16:42:56 -0400

    commit bot

--- server.js
+++ server.js
@@ -4,6 +4,30 @@
 const express = require("express");
 const morgan = require("morgan");
 
+const getBotMessage = (text) => {
+  const commonGreetings = ["hi", "hello", "howdy"];
+  const commonGoodbyes = ["bye", "goodbye", "ciao", "see ya"];
+  let botMsg = "";
+
+  commonGreetings.forEach((greeting) => {
+    if (text.toLowerCase().includes(greeting)) {
+      botMsg = "Hello";
+    }
+  });
+
+  commonGoodbyes.forEach((greeting) => {
+    if (text.toLowerCase().includes(greeting)) {
+      botMsg = "ByeBye";
+    }
+  });
+
+  if (botMsg === "") {
+    botMsg = "Bzzt " + text;
+  }
+
+  return botMsg;
+};
+
 express()
   // Below are methods that are included in express(). We chain them for convenience.
   // --------------------------------------------------------------------------------
@@ -47,6 +71,18 @@ express()
   .get("/parrot-message", (req, res) => {
     const message = { author: "parrot", text: req.query.messageText };
     console.log(req.query);
+    const randomTime = Math.floor(Math.random() * 3000);
+
+    +setTimeout(() => {
+      res.status(200).json({ status: 200, message });
+    }, randomTime);
+  })
+
+  .get("/bot-message", (req, res) => {
+    const responseText = getBotMessage(req.query.messageText);
+
+    const message = { author: "bot", text: responseText };
+
     const randomTime = Math.floor(Math.random() * 3000);
 
     +setTimeout(() => {
